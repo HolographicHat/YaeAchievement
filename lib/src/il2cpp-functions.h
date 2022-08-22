7fa2fbac2524a68c42df977cb5a93dd6f96cd4f6
HolographicHat <holographichat@outlook.com>
2022-08-22 14:25:35 +0800

    3.0 offsets

--- lib/src/il2cpp-functions.h
+++ lib/src/il2cpp-functions.h
@@ -1,10 +1,10 @@
 using namespace Genshin;
 
-DO_APP_FUNC(0x05E24240, 0x04EA10F0, Il2CppString*, Convert_ToBase64String, (ByteArray* value, int offset, int length, void* method));
-DO_APP_FUNC(0x018280A0, 0x018293F0, void, Packet_Xor, (ByteArray** data, int length, void* method));
+DO_APP_FUNC(0x05254960, 0x052544E0, Il2CppString*, Convert_ToBase64String, (ByteArray* value, int offset, int length, void* method));
+DO_APP_FUNC(0x020127B0, 0x02012D40, void, Packet_Xor, (ByteArray** data, int length, void* method));
 
-DO_APP_FUNC(0X00E83770, 0x00E83990, void, MonoLoginMainPage__set_version, (void* obj, Il2CppString* value, void* method));
-DO_APP_FUNC(0x0688D860, 0x058838C0, ByteArray*, Application_RecordUserData, (int32_t nType, void* method));
+DO_APP_FUNC(0X01AD8E40, 0x01AD9740, void, MonoLoginMainPage__set_version, (void* obj, Il2CppString* value, void* method));
+DO_APP_FUNC(0x05C25AC0, 0x05C25E60, ByteArray*, Application_RecordUserData, (int32_t nType, void* method));
 
-DO_APP_FUNC(0x0193BA70, 0x0193C7D0, int, Kcp_Send, (void* client, KcpPacket* pkt, void* method));
-DO_APP_FUNC(0x029EF820, 0x029F05C0, bool, Kcp_Recv, (void* client, ClientKcpEvent* evt, void* method));
+DO_APP_FUNC(0x015C19D0, 0x015C2150, int, Kcp_Send, (void* client, KcpPacket* pkt, void* method));
+DO_APP_FUNC(0x02CF31D0, 0x02CF33A0, bool, Kcp_Recv, (void* client, ClientKcpEvent* evt, void* method));
