204f211249c69a822b6703b50d6ce2b1bc0edc0c
HolographicHat <holographichat@outlook.com>
2022-10-02 13:20:18 +0800

    bump version to 2.2.1

--- src/GlobalVars.cs
+++ src/GlobalVars.cs
@@ -10,7 +10,8 @@ namespace YaeAchievement;
 public static class GlobalVars {
     
     public static bool DebugProxy => false;
-    public static bool UnexpectedExit { get; set; }= true;
+    public static bool UnexpectedExit { get; set; } = true;
+    public static bool PauseOnExit { get; set; } = true;
     public static Version AppVersion { get; } = Assembly.GetEntryAssembly()!.GetName().Version!;
     
     public static readonly string AppPath = AppDomain.CurrentDomain.BaseDirectory;
@@ -19,8 +20,8 @@ public static class GlobalVars {
     public static readonly string CachePath = Path.Combine(DataPath, "cache");
     public static readonly string LibFilePath = Path.Combine(DataPath, "YaeAchievement.dll");
     
-    public const uint   AppVersionCode = 30;
-    public const string AppVersionName = "2.2";
+    public const uint   AppVersionCode = 31;
+    public const string AppVersionName = "2.2.1";
     
     public const string PipeName = "YaeAchievementPipe";
     public const string BucketHost = "https://cn-cd-1259389942.file.myqcloud.com";
