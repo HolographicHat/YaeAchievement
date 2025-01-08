69649e4ade2e37670aa948a24ae52305dc0d5533
HolographicHat <holographichat@outlook.com>
2025-01-08 09:26:48 +0800

    create native lib proj

---
+++ YaeAchievement.NativeLib/src/DllMain.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace YaeAchievement.NativeLib;
+
+public static unsafe class Main {
+
+    [UnmanagedCallersOnly(EntryPoint = "ElysiaMain", CallConvs = [ typeof(CallConvStdcall) ])]
+    private static uint ModuleInit(void* lpThreadParameter) {
+        return 0;
+    }
+}
