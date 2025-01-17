d1bd0c7d7b72aeb85f822fe4ea8b1a79ed4102d5
qhy040404 <qhy040404@163.com>
2025-01-17 21:55:50 +0800

    fix

--- lib/ntprivate.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <Windows.h>
-#include <bcrypt.h>
-
-#pragma comment(lib, "ntdll.lib")
-
-#define LDR_ADDREF_DLL_PIN 0x00000001
-
-EXTERN_C NTSYSAPI NTSTATUS NTAPI LdrAddRefDll(_In_ ULONG Flags, _In_ PVOID DllHandle);
